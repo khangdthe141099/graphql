dc83a69969fbc7cb0d70a651998779b72dd1c740
Dam Tuan Khang <damtuankhangctz@gmail.com>
2024-01-19 16:56:16 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import { readFile } from "node:fs/promises";
+import { resolvers } from "./resolvers.js";
+
+const PORT = 9999;
+
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
+
+app.post("/login", handleLogin);
+
+const typeDefs = await readFile("./schema.graphql", "utf8");
+
+//Define apollo server:
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await apolloServer.start();
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+});
